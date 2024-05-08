6c2e64193e7fd10068380a17b69ab3cf2a78b442
Christianjamesbishop <kriller02@gmail.com>
2024-05-08 09:53:58 +0200

    kommentar login
    kommentar login

--- app/routes/users/login.js
+++ app/routes/users/login.js
@@ -1,8 +1,16 @@
+// Importér Express-frameworket for at oprette en router
 const express = require('express');
+
+// Opret en ny router ved hjælp af Express' router-objekt
 const router = express.Router();
+
+// Importér controlleren, der håndterer login-logikken
 const loginController = require('../../views/controllers/loginController');
 
-// Route for user login:
-router.post('/login', loginController.login); // Add a forward slash before 'login'
+// Definer en POST-rute for '/login'
+// Når en POST-anmodning sendes til denne rute, vil 'login'-metoden i 'loginController' blive kaldt for at håndtere brugerens login
+// Bemærk: Der er tilføjet et fremadskråstreg (/) før 'login' for at sikre, at ruten begynder korrekt
+router.post('/login', loginController.login);
 
+// Eksportér routeren, så den kan bruges i andre dele af applikationen
 module.exports = router;
